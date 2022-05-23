c71133819510c830784fd4c92f55d91bcdbdd7ae
YousefHabeeb21 <105996819+YousefHabeeb21@users.noreply.github.com>
2022-05-22 22:01:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
- /*Program 2: Knight Escape
+ /*
    The objective of this game is to get the white knight to the empty
 	square in the upper right-hand corner of the board.
 	
@@ -7,9 +7,7 @@
 	(two squares in a horizontal direction, then one in a vertical direction OR 
 	two squares in a vertical direction, then one in a horizontal direction).
 		       
-   Course: CS 141, Spring 2022
-   System: 
-   Author: Yousef Habebe
+  
     
  ---------------------------------------------*/
 
